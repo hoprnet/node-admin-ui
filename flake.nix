14f7402d0442ab3834b949772df571d72975c60f
ausias-armesto <ausiasarmesto@gmail.com>
2026-03-26 18:02:24 +0100

    feat: Unify workflows to v2

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "node-admin-ui";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils}:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          pkgsLinux = nixpkgs.legacyPackages."x86_64-linux";
+
+          dockerBuild = pkgs.writeShellApplication {
+              name = "dockerBuild";
+              runtimeInputs = [
+                pkgs.docker
+                pkgs.coreutils
+              ];
+              text = ''
+                #!/usr/bin/env bash
+                set -euo pipefail
+
+                echo "[+] Building: hopr-admin:latest"
+                docker build --platform linux/amd64 -t hopr-admin:latest -f ./Dockerfile .
+                echo "[✓] Done: hopr-admin:latest"
+              '';
+            };
+
+        in
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+
+          # Expose as flake as app
+          apps = {
+            docker-x86_64-linux = {
+              type = "app";
+              program = "${dockerBuild}/bin/dockerBuild";
+            };
+            default = {
+              type = "app";
+              program = "${dockerBuild}/bin/dockerBuild";
+            };
+          };
+        }
+      );
+}
